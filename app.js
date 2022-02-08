24b9c67f06b84f2dbd991ec4f9a4992d7664c47d
Diego Felix <dt196397@gmail.com>
2022-02-08 12:26:28 -0600

    +Main Html structure

---
+++ app.js
@@ -0,0 +1,8 @@
+/*Plan/Add HTML Structure
+- Fill HTML text content
+- Style Flexbox for main structure
+- Create main container
+- Create grid using JS
+- Add color changing interation on JS
+- Add  Interactivity using JS
+- Add Interface with JS
