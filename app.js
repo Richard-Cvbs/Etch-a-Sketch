cbe0f80ef8a2dda89d9039bf33de4191fc8b2b81
Diego Felix <dt196397@gmail.com>
2022-02-09 10:22:28 -0600

    add main Js structure

--- app.js
+++ app.js
@@ -6,12 +6,55 @@
 - Add color changing interation on JS
 - Add  Interactivity using JS
 - Add Interface with JS*/
-const grid=document.querySelector(".grid")
-function createGrid(){
-    for(let i = 0; i<256; i++)
-    let newGridDiv=document.createElement('div')
-    grid.appendChild(newGridDiv)
+const gridContainer = document.querySelector('.grid');
+function createBasicGrid(){
+    for (let i = 0; i<256; i++){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('grid-item')
+        gridContainer.appendChild(newDiv);
+    }
 }
-function question(){
-    console.log("sdfs")
+createBasicGrid();
+function gridListen(){
+    let gridItems = document.querySelectorAll('.grid-item')
+    gridItems.forEach(Element =>{
+        Element.addEventListener('mouseenter', e=>{
+        Element.style.backgroundColor= 'black';
+        })
+    });
 }
+gridListen();
+const resetButton = document.querySelector('.reset')
+function resetGrid(){
+    let gridItems = document.querySelectorAll('.grid-item')
+    resetButton.addEventListener('click', e=>{
+            gridItems.forEach(Element=>{
+        Element.style.backgroundColor= 'White';
+        })
+    });
+}
+resetGrid();
+const gridChangeButtons = document.querySelectorAll('.grid-size-button')
+function gridChange(){
+    gridChangeButtons.forEach(Element =>{
+        Element.addEventListener('click', e=>{
+            removeGridItems();
+            createModifiedGrid(e.target.value);
+        })
+    }
+        )
+}
+gridChange()
+function removeGridItems(){
+    while (gridContainer.lastElementChild) {
+        gridContainer.removeChild(gridContainer.lastElementChild);
+    }
+}
+function createModifiedGrid(value){
+    gridContainer.setAttribute('style', `grid-template-columns: repeat(${value}, 1fr);`); 
+    let augmentedValue = Math.pow(value, 2);
+    for (let i = 0; i<augmentedValue; i++){
+        let newDiv = document.createElement('div');
+        newDiv.className='grid-item';
+        gridContainer.appendChild(newDiv);
+    }
